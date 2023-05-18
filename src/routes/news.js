8abdeac55e17d07b8bec581a2ee2587dddaf13fa
phamtuyenhd202 <anhtuyenhd202@gmail.com>
2023-05-18 09:20:33 +0700

    intall prettier, lint-staged, husky

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
-const newController = require('../app/controllers/newController')
+const newController = require('../app/controllers/newController');
 
-router.use('/:slud', newController.show)
+router.use('/:slud', newController.show);
 router.use('/', newController.index);
 
-module.exports = router
+module.exports = router;
